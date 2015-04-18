7ffeb657730fa7a8c11476af178593b479a0a391
Andrew maiko <andymaik@gmail.com>
2015-04-18 16:34:31 -0500

    Created SiteCommenter and Comment models

--- mysocial/friendapp/models.py
+++ mysocial/friendapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class SiteCommenter(models.Model):
+    user = models.OneToOneField(User)
+    
+    def __unicode__(self):
+        return self.name
+
+class Comment(models.Model):
+    siteCommenter = models.ForeignKey(SiteCommenter)
+    comment_text = models.CharField(max_length=300)
+
+    def __unicode__(self):
+        return self.name
