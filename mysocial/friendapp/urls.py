f18b5bf97e8bfaeb44e96b6912da0b14e06aef52
Andrew maiko <andymaik@gmail.com>
2015-05-17 16:31:39 -0500

    add static/friendapp/Windows-7-Logo-Wallpaper.jpg and static template

--- mysocial/friendapp/urls.py
+++ mysocial/friendapp/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, url
-from polls import views
-
- urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+from friendapp import views
+urlpatterns = patterns('',
+  url(r'^$',views.index, name='index'),
  )
--- mysocial/friendapp/views.py
+++ mysocial/friendapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from friendapp.models import User, Comment
-   def index(request):
-              myuser = User.objects.filter(id=2)
-              context = {'myuser': myuser}
-              return render(request, 'friendsapp/index.html', context)
+
+def index(request):
+             myuser = User.objects.filter(id=2)
+             context = {'myuser': myuser}
+             return render(request, 'friendapp/index.html', context)
