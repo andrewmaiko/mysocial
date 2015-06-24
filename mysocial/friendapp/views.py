17803a295478dae9720ff01db67487577d72d7ad
Andrew maiko <andymaik@gmail.com>
2015-06-23 21:00:40 -0500

    implementing solution to obtain list of all plots in static directory and then use a for loop index.html to display them

--- mysocial/friendapp/views.py
+++ mysocial/friendapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from friendapp.models import User, Comment
+import os.path
 
 def index(request):
-             myuser = User.objects.filter(id=2)
-             context = {'myuser': myuser}
+            # myuser = User.objects.filter(id=2)
+             plots = [('friendapp/' + plot) for plot in os.listdir('C:/Users/Andrew/Desktop/my_files/djangoweb/mysocial/mysocial/friendapp/static/friendapp') if os.path.isfile(os.path.join('C:/Users/Andrew/Desktop/my_files/djangoweb/mysocial/mysocial/friendapp/static/friendapp',plot))]
+             context = {'plots': plots}
              return render(request, 'friendapp/index.html', context)
